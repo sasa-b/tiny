9bd22419d139361f7d9c5f8cac61e11cdf1c2535
Sasa B <sasa.blagojevic@mail.com>
2017-07-12 23:57:38 +0200

    Complete rewrite.
    Added hash and history api routing capabilities

--- router.js
+++ router.js
@@ -1,182 +1,215 @@
-/**
- * Created by Sasa B. on 25-Jul-16.
- */
-var Tiny = function () {
-    var thisObj = this;
-    this.url = window.location.href;
-    this.location = '';
-    this.oldHash = '';
-    this.routes = [];
-    this.routeParams = [];
+function Router(config) {
+    var self = this;
 
-    this.defaultRoute = {
-        "hash" : '',
-        "method" : null
+    var routes = {
+        hash: [],
+        url: []
     };
 
-    this.setDefaultRouteMethod = function(callback) {
-        thisObj.defaultRoute.method = callback;
-        thisObj.routes.push(thisObj.defaultRoute);
+    var actions = {};
+
+    var patterns = {
+        hash: [],
+        url: []
+    };
+
+    var matchedHash = "";
+
+    this.defaultHashAction = null;
+
+    this.className = 'push-state';
+
+    this.trimSlashes = function(string) {
+        return string.replace(/^\/+/, '').replace(/\/+$/, '');
     }
 
-    this.parseUrl = function() {
-        var url = location.href;
-
-        url = url.substr(7, url.length);
-        url = url.split('/');
-
-        return url;
+    var parseRoute = function (hash) {
+        hash = self.trimSlashes(hash);
+        hash = hash.replace('/', '\/');
+        hash = hash.replace(/\{\w+\}/g, '([a-zA-Z0-9-]+)');
+        return '^' + hash + '$';
     }
 
-    this.setRoute = function(route, callback) {
-        var pattern = /({[a-zA-z0-9]+})/g;
-        if (pattern.test(route) == true) {
-            var params = route.split(':');
-            var hash = params[0];
-                params.splice(0, 1);
-            var l = params.length;
+    var getParams = function(matches) {
+        delete matches['index'];
+        delete matches['input'];
+        return matches.slice(1, matches.length);
+    }
+
+    var pushState = function (state, path) {
+        history.pushState(state ? state : null, state && state.hasOwnProperty('title') ? state.title : document.title, path);
+        var event = new Event("popstate");
+            event.state = state;
+        window.dispatchEvent(event);
+    }
+
+    this.getPath = function(){
+        var location = window.location.href.replace(/\/+$/, '').split('//')[1].split('/');
+        return location.slice(1, location.length).join("/");
+    }
+
+    this.addHash = function (route, callback) {
+        routes.hash.push(route);
+
+        var pattern = parseRoute(route);
+            patterns.hash.push(pattern);
+
+        actions[pattern] = callback;
+        return this;
+    }
+
+    this.addRoute = function(route, callback) {
+        routes.url.push(route);
+
+        var pattern = parseRoute(route.replace(/#+$/, ''));
+            patterns.url.push(pattern);
+
+        actions[pattern] = callback;
+        return this;
+    }
+
+    this.getRoutes = function () {
+        return routes;
+    }
+
+    this.getRoute = function (key) {
+        try {
+            if (typeof key === 'number') {
+                return routes.url[key];
+            } else {
+                var l = routes.url.length;
+                for (var i = 0; i < l; i++) {
+                    if (key === routes.url[i]) {
+                        return routes.url[i];
+                    }
+                }
+            }
+        } catch (e) {
+            return null;
+        }
+    }
+
+    this.getHashRoute = function (key) {
+        try {
+            if (typeof key === 'number') {
+                return routes.hash[key];
+            } else {
+                var l = routes.hash.length;
+                for (var i = 0; i < l; i++) {
+                    if (key === routes.hash[i]) {
+                        return routes.hash[i];
+                    }
+                }
+            }
+        } catch (e) {
+            return null;
+        }
+    }
+
+    this.getRouteAction = function(route) {
+        return actions[parseRoute(route)];
+    }
+
+    this.hashMatch = function () {
+        if (window.location.hash === "") {
+            if (this.defaultHashAction) {
+                this.defaultHashAction();
+            }
+            return true;
+        }
+
+        var l = patterns.hash.length;
+        for (var i = 0; i < l; i++) {
+            var matches = window.location.hash.match(new RegExp(patterns.hash[i]));
+            if (matches) {
+                actions[patterns.hash[i]].apply(this, getParams(matches));
+                return true;
+            }
+        }
+        return false;
+    }
+
+    this.hashMatchCompatMode = function() {
+        if (window.location.hash !== matchedHash) {
+            if (self.hashMatch()) {
+                matchedHash = window.location.hash;
+            }
+        }
+    }
+
+    this.urlMatch = function () {
+        var l = patterns.url.length;
+        for (var i = 0; i < l; i++) {
+            var matches = self.getPath().match(new RegExp(patterns.url[i]));
+            console.log(self.getPath(), patterns.url[i]);
+            if (matches) {
+                actions[patterns.url[i]].apply(this, getParams(matches));
+                return true;
+            }
+        }
+        return false;
+    }
+    
+    this.listen = function () {
+        if ("onhashchange" in window) {
+            window.onhashchange = self.hashMatch;
+        } else {
+            window.setInterval(self.hashMatchCompatMode, 150);
+        }
+
+       try {
+           window.addEventListener('popstate', function(e) {
+               if (self.urlMatch()) {
+                   e.preventDefault();
+               }
+           });
+       } catch (e) {
+           throw new Error("Browser does not support History Api, please update your browser.");
+       }
+    }
+
+    this.cache = function (callback, uniqueId) {
+        if (typeof uniqueId === 'undefined') {
+            uniqueId = '';
+        }
+
+        var cached = localStorage.getItem('_router'+uniqueId);
+        if (!cached) {
+            callback();
+            localStorage.setItem('_router'+uniqueId, JSON.stringify({"routes": routes, "actions": actions, "patterns": patterns}));
+        }
+
+        routes = cached["routes"];
+        patterns = cached["patterns"];
+        actions = cached["actions"];
+    }
+
+    var construct = function (config) {
+        if (config && config.hasOwnProperty('hybrid') && config['hybrid'] === true) {
+            var a = document.getElementsByClassName(self.className);
+            var l = a.length;
             for (var i = 0; i < l; i++) {
-                params[i] = params[i].replace(/[^a-zA-z0-9]+/g, '');
-            }
-            var newRoute = {
-                "hash" : hash,
-                "method" : callback,
-                "params" : params
-            };
-        } else {
-            var newRoute = {
-                "hash" : route,
-                "method" : callback,
-            };
-        }
-        thisObj.routes.push(newRoute);
-    }
-
-    this.compatMode = function() {
-        var url = thisObj.parseUrl();
-        var hash = thisObj.parseRoute(location.hash);
-
-        if (hash) {
-            if (thisObj.oldHash !== hash) {
-                thisObj.oldHash = hash;
-                thisObj.routes.forEach(function(route) {
-                    if (route.hash === hash) {
-                        thisObj.location = route.hash;
-                        if (route.hasOwnProperty('params')) {
-                            route.method.apply(this, thisObj.routeParams);
-                        } else {
-                            route.method();
-                        }
-                        console.log(route);
-                    }
+                a[i].addEventListener('click', function (e) {
+                    e.preventDefault();
+                    pushState(this.dataset.pushState, this.href);
                 });
             }
         } else {
-            url.forEach(function(part) {
-                if (part.indexOf('#') > -1) {
-                    var hash = part.substr(part.indexOf('#'), part.length);
-                        hash = thisObj.parseRoute(hash);
-                    console.log(hash);
-                    if (thisObj.oldHash !== hash) {
-                        thisObj.oldHash = hash;
-                        thisObj.routes.forEach(function(route) {
-                            if (route.hash === hash) {
-                                thisObj.location = route.hash;
-                                if (route.hasOwnProperty('params')) {
-                                    route.method.apply(this, thisObj.routeParams);
-                                } else {
-                                    route.method();
-                                }
-                                console.log(route);
-                            }
-                        });
-                    }
-
-                }
-            });
-        }
-    }
-
-    this.parseRoute = function(route) {
-        if (route.indexOf(':') > -1) {
-            var params = route.split(':');
-            var hash = params[0];
-            params.splice(0, 1);
-
-            thisObj.routeParams = params;
-
-            return hash;
-        }
-
-        return route;
-    }
-
-    this.listen = function() {
-
-       if ("onhashchange" in window) {
-            window.onhashchange = function() {
-
-                var hash = thisObj.parseRoute(location.hash);
-
-                thisObj.routes.forEach(function(route) {
-                    if (route.hash === hash) {
-                        thisObj.location = route.hash;
-                        if (route.hasOwnProperty('params')) {
-                            route.method.apply(this, thisObj.routeParams);
-                        } else {
-                            route.method();
-                        }
-                        console.log(route);
+            var a = document.getElementsByTagName('a');
+            var l = a.length;
+            for (var i = 0; i < l; i++) {
+                a[i].addEventListener('click', function (e) {
+                    if (this.href.indexOf('#') > 0) {
+                        e.preventDefault();
+                        pushState(this.dataset.pushState, this.href);
                     }
                 });
             }
-        } else {
-            window.setInterval(thisObj.compatMode, 100);
         }
-    }
+    }(config);
 
-    this.onPageLoad = function() {
-        var hash = thisObj.parseRoute(location.hash);
-        thisObj.location = hash;
-
-        if (!hash && hash != '') {
-            var url = thisObj.parseUrl();
-            console.log(url);
-            url.forEach(function(part) {
-                if (part.indexOf('#') > -1) {
-                    var hash = part.substr(part.indexOf('#'), part.length);
-                    hash = thisObj.parseRoute(hash);
-                    thisObj.oldHash = hash;
-                    console.log(hash);
-
-                    thisObj.routes.forEach(function(route) {
-                        if (route.hasOwnProperty('params')) {
-                            route.method.apply(this, thisObj.routeParams);
-                        } else {
-                            route.method();
-                        }
-                    });
-
-                }
-            });
-            return false;
-        }
-
-        thisObj.routes.forEach(function(route) {
-            if (route.hash === hash) {
-                if (route.hasOwnProperty('params')) {
-                    route.method.apply(this, thisObj.routeParams);
-                } else {
-                    route.method();
-                }
-            }
-        });
-
-    }
-
-    window.addEventListener('DOMContentLoaded', function() {
-        console.log('onLoad - hashevent');
-        thisObj.onPageLoad();
+    window.addEventListener('load', function () {
+        self.urlMatch();
+        self.hashMatch();
     });
-
 }
